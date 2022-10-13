d9f076bfcfedd554a2bd1b616646bb0fa99621bd
rupsha014 <rupshasarkar14@gmail.com>
2022-10-13 00:38:12 -0700

    1s commit

--- faq-accordion-card-main/index.js
+++ faq-accordion-card-main/index.js
@@ -4,6 +4,11 @@ const answ2 = document.getElementsByClassName("ans2")[0];
 const answ3 = document.getElementsByClassName("ans3")[0];
 const answ4 = document.getElementsByClassName("ans4")[0];
 const answ5 = document.getElementsByClassName("ans5")[0];
+const que1=document.getElementsByClassName("q1")[0];
+const que2=document.getElementsByClassName("q2")[0];
+const que3=document.getElementsByClassName("q3")[0];
+const que4=document.getElementsByClassName("q4")[0];
+const que5=document.getElementsByClassName("q5")[0];
 const pointer1 = document.getElementsByClassName("point")[0];
 const pointer2 = document.getElementsByClassName("point")[1];
 const pointer3 = document.getElementsByClassName("point")[2];
@@ -16,55 +21,67 @@ const pointerup4 = document.getElementsByClassName("point-up")[3];
 const pointerup5 = document.getElementsByClassName("point-up")[4];
 
 pointer1.addEventListener("click" ,() => {
+    document.getElementsByClassName('q1')[0].classList.add('bold')
     document.getElementsByClassName('ans1')[0].classList.remove('hidden');  
     pointer1.classList.add('hidden');
     pointerup1.classList.remove('hidden')
+    
+    
 });
 pointerup1.addEventListener("click" ,() => {
+    document.getElementsByClassName('q1')[0].classList.remove('bold')
     document.getElementsByClassName('ans1')[0].classList.add('hidden');  
     pointer1.classList.remove('hidden');
     pointerup1.classList.add('hidden')
 });
 
 pointer2.addEventListener("click" ,() => {
+    document.getElementsByClassName('q2')[0].classList.add('bold')
     document.getElementsByClassName('ans2')[0].classList.remove('hidden');  
     pointer2.classList.add('hidden');
     pointerup2.classList.remove('hidden')
 });
 pointerup2.addEventListener("click" ,() => {
+    document.getElementsByClassName('q2')[0].classList.remove('bold')
     document.getElementsByClassName('ans2')[0].classList.add('hidden');  
     pointer2.classList.remove('hidden');
     pointerup2.classList.add('hidden')
 });
 
 pointer3.addEventListener("click" ,() => {
+    document.getElementsByClassName('q3')[0].classList.add('bold')
     document.getElementsByClassName('ans3')[0].classList.remove('hidden');   
     pointer3.classList.add('hidden');
     pointerup3.classList.remove('hidden')
 });
 pointerup3.addEventListener("click" ,() => {
+    document.getElementsByClassName('q3')[0].classList.remove('bold')
     document.getElementsByClassName('ans3')[0].classList.add('hidden');  
     pointer3.classList.remove('hidden');
     pointerup3.classList.add('hidden')
 });
 
 pointer4.addEventListener("click" ,() => {
+    document.getElementsByClassName('q4')[0].classList.add('bold')
     document.getElementsByClassName('ans4')[0].classList.remove('hidden'); 
     pointer4.classList.add('hidden');
     pointerup4.classList.remove('hidden')
 });
 pointerup4.addEventListener("click" ,() => {
+    document.getElementsByClassName('q4')[0].classList.remove('bold')
     document.getElementsByClassName('ans4')[0].classList.add('hidden');  
     pointer4.classList.remove('hidden');
     pointerup4.classList.add('hidden')
 });
 
 pointer5.addEventListener("click" ,() => {
+    document.getElementsByClassName('q5')[0].classList.add('bold')
     document.getElementsByClassName('ans5')[0].classList.remove('hidden');
     pointer5.classList.add('hidden');
     pointerup5.classList.remove('hidden')
 });
 pointerup5.addEventListener("click" ,() => {
+    document.getElementsByClassName('q5')[0].classList.remove('bold')
     document.getElementsByClassName('ans5')[0].classList.add('hidden');  
     pointer5.classList.remove('hidden');
     pointerup5.classList.add('hidden')
